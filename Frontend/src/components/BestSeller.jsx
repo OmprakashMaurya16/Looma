a89994723047c39b8c1acb732cbd561399dc7c48
Omprakash Maurya <omprakash.maurya1604@gmail.com>
2025-09-08 16:25:05 +0530

    BestSeller Component

---
+++ Frontend/src/components/BestSeller.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useState } from "react";
+import ShopContext from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+const BestSeller = () => {
+  const { products } = useContext(ShopContext);
+  console.log(products);
+
+  const [bestSellers, setBestSellers] = useState([]);
+
+  useEffect(() => {
+    const filtered = products.filter((item) => item.bestseller);
+    setBestSellers(filtered.slice(0, 5));
+  }, [products]);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"BEST"} text2={"SELLERS"} />
+        <p className="w-3/4 m-auto text-xs text-gray-600 sm:text-sm md:text-base">
+          Don’t miss out! These trending best sellers are in high demand and
+          selling fast—grab yours while they last.
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSellers.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
