b43c1a85aed6e35add91a1e7c8dc548f73b71b84
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2025-11-26 13:15:27 +0530

    Initial Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import CollectionPage from "./pages/CollectionPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import CartPage from "./pages/CartPage.jsx";
+import PlaceOrderPage from "./pages/PlaceOrderPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import OrderPage from "./pages/OrderPage.jsx";
+import Navbar from "./components/Navbar.jsx";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/product/:productId" element={<ProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/place-order" element={<PlaceOrderPage />} />
+        <Route path="/order" element={<OrderPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
