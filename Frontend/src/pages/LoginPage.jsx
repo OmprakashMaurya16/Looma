f4c01fd6d3202b27a3323ed8a6770620e490511b
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2025-11-28 19:27:12 +0530

    Complete Login page

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [currentState, setCurrentState] = useState("SignUp");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+  };
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800"
+    >
+      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+        <p className="prata-regular text-3xl">{currentState}</p>
+        <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
+      </div>
+      {currentState === "Login" ? (
+        ""
+      ) : (
+        <input
+          type="text"
+          name="name"
+          className="w-full px-3 py-2 border border-gray-800"
+          placeholder="Enter your name"
+          required
+        />
+      )}
+      <input
+        type="email"
+        name="email"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Enter your email"
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        className="w-full px-3 py-2 border border-gray-800"
+        placeholder="Enter your password"
+        required
+      />
+      <div className="w-full flex justify-between text-sm -mt-2">
+        <p>Forget your password?</p>
+        {currentState === "Login" ? (
+          <p
+            onClick={() => setCurrentState("SignUp")}
+            className="cursor-pointer"
+          >
+            Create account
+          </p>
+        ) : (
+          <p
+            onClick={() => setCurrentState("Login")}
+            className="cursor-pointer"
+          >
+            Login Here
+          </p>
+        )}
+      </div>
+      <button className="bg-black text-white font-light px-8 py-2 mt-4 w-full">
+        {currentState === "Login" ? "Sign In" : "Sign Up"}
+      </button>
+    </form>
+  );
 };
 
 export default LoginPage;
