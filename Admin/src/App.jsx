4dc5c4241158828095feec6b4ac7d6b1d36df365
OmprakashMaurya16 <omprakash.maurya1604@gmail.com>
2025-12-01 05:01:41 +0530

    Navbar & Sidebar

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./Sidebar";
+import { Routes, Route } from "react-router-dom";
+import AddPage from "./pages/AddPage";
+import ListPage from "./pages/ListPage";
+import OrdersPage from "./pages/OrdersPage";
+import Login from "./components/Login";
 
 const App = () => {
-  return <div>App</div>;
+  const [token, setToken] = useState("");
+
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<AddPage />} />
+                <Route path="/list" element={<ListPage />} />
+                <Route path="/orders" element={<OrdersPage />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
